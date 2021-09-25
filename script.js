6548fcbc6f0696f801db67adee2140b0b52d3b3c
Zak <zak.solowsky@gmail.com>
2021-09-24 23:22:14 -0400

    added working slider to set grid size - removed prompt to set gridNumber

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 const gridContainer = document.querySelector("#gridContainer");
-  const erase = document.getElementById("erase");
-//prompt set gridBox size 
-gridNumber = prompt("Set the Box legnth - <= 100", "Number Here")
-boxSize = 800 / gridNumber + "px"
+  const erase = document.getElementById("erase");  
+const slider = document.getElementById("slider");
+const sliderAmount = document.getElementById("sliderAmount");
+  const reset = document.getElementById("reset");
 
-//
+sliderAmount.innerHTML = slider.value+"x"+slider.value;
+sliderNumber = slider.value;
+
+slider.onchange =function(){sliderAmount.innerHTML= slider.value+"x"+slider.value;
+drawGrid()}
+
+
+
+
+//prompt set gridBox size 
+
+let drawGrid = function () {
+
+
+
+
+const allRows = document.querySelectorAll("#gridRow")
+
 const gridRow = document.createElement('div');
 gridRow.setAttribute("id", "gridRow");
 const gridBox = document.createElement('div');
 gridBox.setAttribute("id", "gridBox");
 
-gridBox.style.height=boxSize
-gridBox.style.width=boxSize
+//Resets grid if allRows value is greater than zero
+for( i = 0; i < allRows.length; i++)
+{allRows[i].remove();}
+// 
+
+gridNumber = slider.value;
+
+boxSize = 800 / gridNumber + "px";
+
+gridBox.style.height=boxSize;
+gridBox.style.width=boxSize;
 
 
 // CREATES GRID based on gridNumber
@@ -21,6 +47,7 @@ gridBox.style.width=boxSize
 for ( i = 0; i < gridNumber; i++)
  { gridContainer.appendChild(gridRow.cloneNode(true))
 };
+
 
 /// all boxes nodelist
 let boxList = document.querySelectorAll("#gridBox");
@@ -50,16 +77,15 @@ for (i = 0; i < boxList.length; i++)
 //   currentBox.addEventListener("mouseover", function(){ 
 //      currentBox.style.backgroundColor="white"})})
 
+ function resetPage() {currentBox.style.backgroundColor="white"};
+ 
 
-  const reset = document.getElementById("reset");
-  reset.addEventListener("click", function() {currentBox.style.backgroundColor="white"} )
-
+  // reset.addEventListener("click", function() {currentBox.style.backgroundColor="white"} )
+reset.addEventListener("click", resetPage)
 
 
   // erase.addEventListener("click", function(){ 
-    }
-
-
+    }};
 
 
 // Box size:
